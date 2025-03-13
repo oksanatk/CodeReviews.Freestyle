ab2b6864a26e15211b9c02743e287e222cd89bdb
oksanatk <tkachoksn@gmail.com>
2025-03-13 16:12:02 -0400

    Flashcards app from local files

---
+++ Flashcards.oksanatk/Models/FlashcardDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnetMAUI.Flashcards.Models;
+
+public class FlashcardDto
+{
+    [Key]
+    public int Id { get; set; }
+    public Stack Stack { get; set; } = null!;
+    public int DisplayNum { get; set; }
+    public string Front { get; set; } = null!;
+    public string Back { get; set; } = null!;
+}
---
+++ Flashcards.oksanatk/Models/StudySessionPivotDto.cs
@@ -0,0 +1,7 @@
+﻿namespace dotnetMAUI.Flashcards.Models;
+
+public class StudySessionPivotDto
+{
+    public string StackName { get; set; } = null!;
+    public Dictionary<string, int> MonthlyCounts { get; set; } = new();
+}
